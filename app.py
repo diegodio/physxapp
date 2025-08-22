15e219de5d07aa3aefa1275b3e800ea788621f40
diegodio <diego__dio@hotmail.com>
2025-08-22 14:38:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 import streamlit as st
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 st.set_page_config(page_title="Auth demo")
 st.title("Authentication")
+
+LINK = "https://physxapp-3dbf5-default-rtdb.firebaseio.com/"
+keep_login_time = False
+
+def save_login_time(link, sub, nome):
+    # Criar uma venda (POST)
+    dados = {'login_time': (datetime.now() - timedelta(hours=3)).strftime("%d/%m/%Y %H:%M:%S")}
+    requisicao = requests.post(f'{link}/{sub + ' - ' + nome}/logininfo/.json', data=json.dumps(dados))
+    # print(requisicao)
+    # print(requisicao.text)
+
+
 
 # Se não estiver logado (ou se o atributo nem existir), aciona login
 if not getattr(st.user, "is_logged_in", False):
     if st.button("Login com Google"):
         st.login("google")   # redireciona para o IdP configurado
-    st.stop()
+        keep_login_time = True
+        st.stop()
+        
+    # 
+else:
+    if keep_login_time:
+        save_login_time(link=LINK, sub = st.user.sub, nome=st.user.name)
+        keep_login_time = False
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
+
+
 
 # Daqui para baixo: usuário autenticado
 st.success("Autenticado!")
@@ -22,14 +47,4 @@ if picture:
     st.image(picture, width=120)
 
 
-
-sub = st.user.sub
-
-LINK = "https://physxapp-3dbf5-default-rtdb.firebaseio.com/"
-
-# Criar uma venda (POST)
-dados = {'login_time': datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
-requisicao = requests.post(f'{LINK}/{sub}/logininfo/.json', data=json.dumps(dados))
-print(requisicao)
-print(requisicao.text)
 
