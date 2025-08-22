8e1dcb0dc5b14fbad1e8a9a479d13249bdad45b5
diegodio <diego__dio@hotmail.com>
2025-08-22 08:51:18 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Authentication")
+
+if not st.experimental_user.is_logged_in:
+    if st.button("Authenticate"):
+        st.login("google")
+
+st.json(st.experimental_user)
+st.header(f"Hello, {st.experimental_user.name}")
+st.image(st.experimental_user.picture)
