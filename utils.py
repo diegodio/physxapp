4cb8adb6b0f1a6d65040f5169b33f66c0bc0a783
diegodio <diego__dio@hotmail.com>
2025-09-05 15:46:51 -0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import json
 from info import *
-from app import *
+# from app import *
 
 LINK = "https://physxapp-3dbf5-default-rtdb.firebaseio.com/"
 
@@ -52,43 +52,44 @@ def firstLogIn():
 
 def save_acertos_erros(acertos, erros):
     if st.session_state.new_load == True:
-        sub = getattr(st.user, "sub", "sub_x")
-        name = emails_alunos_1MC[getattr(st.user, "email", "email_x")]
-        turma = getTurma(getattr(st.user, "email", "email_x"))
+        st.session_state.new_load = False
 
-        try:
-            requisicao = requests.get(f'{LINK}/{turma}/{name}/acertos_erros_fr.json')
-            dic_requisicao = requisicao.json()
+    sub = getattr(st.user, "sub", "sub_x")
+    name = emails_alunos_1MC[getattr(st.user, "email", "email_x")]
+    turma = getTurma(getattr(st.user, "email", "email_x"))
 
-            lista_keys = list(dic_requisicao.keys()) or []
+    try:
+        requisicao = requests.get(f'{LINK}/{turma}/{name}/acertos_erros_fr.json')
+        dic_requisicao = requisicao.json()
 
-            # print(lista_keys)
-            # Deletar uma venda (DELETE)
-            for key in lista_keys:
-                url_exclude = f'{LINK}/{turma}/{name}/acertos_erros_fr/{key}.json'
-                requisicao = requests.delete(url_exclude)
-                # print(requisicao)
-                # print(requisicao.text) 
-                porcentagem = (100 * acertos / (acertos+erros)) if (acertos+erros) > 0 else 0
+        lista_keys = list(dic_requisicao.keys()) or []
 
-                nota = (porcentagem/100)*40
-                
-                # Editar a venda (PATCH)
-                dados = {'acertos': acertos, 'erros': erros, 'porcentagem': porcentagem, 'nota': nota}
-                requisicao = requests.post(f'{LINK}/{turma}/{name}/acertos_erros_fr.json', data=json.dumps(dados))  
-        except:
-            pass
+        # print(lista_keys)
+        # Deletar uma venda (DELETE)
+        for key in lista_keys:
+            url_exclude = f'{LINK}/{turma}/{name}/acertos_erros_fr/{key}.json'
+            requisicao = requests.delete(url_exclude)
+            # print(requisicao)
+            # print(requisicao.text) 
+            porcentagem = (100 * acertos / (acertos+erros)) if (acertos+erros) > 0 else 0
+
+            nota = (porcentagem/100)*40
+            
+            # Editar a venda (PATCH)
+            dados = {'acertos': acertos, 'erros': erros, 'porcentagem': porcentagem, 'nota': nota}
+            requisicao = requests.post(f'{LINK}/{turma}/{name}/acertos_erros_fr.json', data=json.dumps(dados))  
+    except:
+        pass
 
 
-        porcentagem = (100 * acertos / (acertos+erros)) if (acertos+erros) > 0 else 0
+    porcentagem = (100 * acertos / (acertos+erros)) if (acertos+erros) > 0 else 0
 
-        nota = (porcentagem/100)*40
-        
-        # Editar a venda (PATCH)
-        dados = {'acertos': acertos, 'erros': erros, 'porcentagem': porcentagem, 'nota': nota}
-        requisicao = requests.post(f'{LINK}/{turma}/{name}/acertos_erros_fr.json', data=json.dumps(dados))
+    nota = (porcentagem/100)*40
+    
+    # Editar a venda (PATCH)
+    dados = {'acertos': acertos, 'erros': erros, 'porcentagem': porcentagem, 'nota': nota}
+    requisicao = requests.post(f'{LINK}/{turma}/{name}/acertos_erros_fr.json', data=json.dumps(dados))
 
-    st.session_state.new_load = False
 
 def conferir_resposta(resposta_correta, resposta_dada):
     if resposta_correta == resposta_dada:
@@ -102,5 +103,5 @@ def conferir_resposta(resposta_correta, resposta_dada):
         #     st.session_state.erros = 0
         
         st.session_state.erros += 1
-
+    
     save_acertos_erros(acertos = st.session_state.acertos, erros = st.session_state.erros)
